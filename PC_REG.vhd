8e9313bdceb1817b7df301cbb0fe31662729b34a
rseranllari <59234808+rseranllari@users.noreply.github.com>
2019-12-26 13:42:14 -0600

    Add files via upload

---
+++ PC_REG.vhd
@@ -0,0 +1,25 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
+
+entity PC_REG is
+	port(
+		clk : in std_logic;
+		reset : in std_logic;
+		Input : in std_logic_vector(15 downto 0);
+		Output : out std_logic_vector(15 downto 0)
+	);
+end PC_REG;
+
+architecture Behavioral of PC_REG is
+begin
+	process(reset, clk)
+	begin
+		if (reset = '0') then
+			output <= x"0000";
+		elsif (clk'event and clk='1') then
+			Output <= Input;
+		end if; 
+	end process;
+end Behavioral;
